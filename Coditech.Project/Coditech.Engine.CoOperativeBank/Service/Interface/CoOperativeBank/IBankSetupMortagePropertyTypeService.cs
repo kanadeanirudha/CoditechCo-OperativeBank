8aa65426509ccfbd9ad0a31442e77d03f7ce7364
Bhavesh Shambharkar <148750630+BhaveshS777@users.noreply.github.com>
2025-04-09 17:22:58 +0530

    Code Correction

---
+++ Coditech.Project/Coditech.Engine.CoOperativeBank/Service/Interface/CoOperativeBank/IBankSetupMortagePropertyTypeService.cs
@@ -0,0 +1,14 @@
+﻿using Coditech.Common.API.Model;
+using Coditech.Common.Helper.Utilities;
+using System.Collections.Specialized;
+namespace Coditech.API.Service
+{
+    public interface IBankSetupMortagePropertyTypeService
+    {
+        BankSetupMortagePropertyTypeListModel GetPropertyTypeList(FilterCollection filters, NameValueCollection sorts, NameValueCollection expands, int pagingStart, int pagingLength);
+        BankSetupMortagePropertyTypeModel CreatePropertyType(BankSetupMortagePropertyTypeModel model);
+        BankSetupMortagePropertyTypeModel GetPropertyType(short bankSetupMortagePropertyTypeId);
+        bool UpdatePropertyType(BankSetupMortagePropertyTypeModel model);
+        bool DeletePropertyType(ParameterModel parameterModel);
+    }
+}
